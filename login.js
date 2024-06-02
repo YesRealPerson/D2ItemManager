0c7d3b1a5f9328b4c009c2034d8b314259d7c06c
YesRealPerson <Stephen-park@outlook.com>
2024-06-02 15:19:59 -0700

    god damn it

--- login.js
+++ login.js
@@ -2,5 +2,5 @@ document.getElementById('log').addEventListener("click", () => {
     // generate randomized state parameter
     let state = Math.floor((Math.random*1000000));
     // Redirect to bungie auth page
-    window.location.href = "https://www.bungie.net/en/OAuth/Authorize?client_id=40053&responsetype=code&state="+state;
+    window.location.href = "https://www.bungie.net/en/OAuth/Authorize?client_id=47120&response_type=code&state="+state;
 })
