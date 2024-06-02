99afb8bcb235fa9e874ee34a18bf4e1fbe2bbf2b
YesRealPerson <Stephen-park@outlook.com>
2024-06-02 16:44:05 -0700

    Maybe

--- auth.js
+++ auth.js
@@ -1,6 +1,9 @@
-//Get parameters from URL
-const params = new URLSearchParams(window.location.search);
-//Send OAuth token request
-const response = await fetch("https://d2oauth.spark952.workers.dev?code="+params.get("code"));
-
-console.log(response);
+const main = async () => {
+    //Get parameters from URL
+    const params = new URLSearchParams(window.location.search);
+    //Send OAuth token request
+    const response = JSON.parse(await (await fetch("https://d2oauth.spark952.workers.dev?code=" + params.get("code"))).text());
+    
+    console.log(response);
+}
+main();
--- login.js
+++ login.js
@@ -1,6 +1,15 @@
+const generateState = () => {
+    let state = "";
+    let possible = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+    for(let i = 0; i < 20; i++){
+        state += possible.charAt(Math.floor(Math.random()*possible.length))
+    }
+    return state;
+}
+
 document.getElementById('log').addEventListener("click", () => {
     // generate randomized state parameter
-    let state = Math.floor((Math.random*1000000));
+    let state = generateState();
     // Redirect to bungie auth page
     window.location.href = "https://www.bungie.net/en/OAuth/Authorize?client_id=47120&response_type=code&state="+state;
 })
