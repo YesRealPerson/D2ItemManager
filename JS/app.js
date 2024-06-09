074cebf43baf492b0cd566ab01b4bfd88deaba4a
YesRealPerson <Stephen-park@outlook.com>
2024-06-09 11:29:05 -0700

    added app redirect

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+// OAuth Access & Refresh Token
+const access = localStorage.getItem("access");
+const refresh = localStorage.getItem("refresh");
+// Base account ID
+const accID = localStorage.getItem("accountID");
+
+// Main function loop
+(async function (){
+    // Get manifests
+})();
--- auth.js
+++
@@ -1,9 +0,0 @@
-const main = async () => {
-    //Get parameters from URL
-    const params = new URLSearchParams(window.location.search);
-    //Send OAuth token request
-    const response = JSON.parse(await (await fetch("https://d2oauth.spark952.workers.dev?code=" + params.get("code"))).text());
-    
-    console.log(response);
-}
-main();
