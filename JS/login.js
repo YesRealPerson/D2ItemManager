94523e0244e4e31ad6622032dc78f88703f5c681
YesRealPerson <Stephen-park@outlook.com>
2024-09-09 19:34:37 -0700

    minified JS

--- JS/login.js
+++ JS/login.js
@@ -1,15 +1,1 @@
-const generateState = () => {
-    let state = "";
-    let possible = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
-    for(let i = 0; i < 20; i++){
-        state += possible.charAt(Math.floor(Math.random()*possible.length))
-    }
-    return state;
-}
-
-document.getElementById('log').addEventListener("click", () => {
-    // generate randomized state parameter
-    let state = generateState();
-    // Redirect to bungie auth page
-    window.location.href = "https://www.bungie.net/en/OAuth/Authorize?client_id=47120&response_type=code&state="+state;
-})
+const generateState=()=>{let e="",t="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";for(let n=0;n<20;n++)e+=t.charAt(Math.floor(Math.random()*t.length));return e};document.getElementById("log").addEventListener("click",()=>{let e=generateState();window.location.href="https://www.bungie.net/en/OAuth/Authorize?client_id=47120&response_type=code&state="+e});
